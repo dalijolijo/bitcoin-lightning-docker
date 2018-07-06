bcb1d361703f52d2c56959fb2011500e86a0d3f4
Pierre Rochard <pierre@rochard.org>
2018-07-06 14:45:50 -0400

    fix channel creation

--- services/admin_website/app/app/lnd_client/admin/channels_model_view.py
+++ services/admin_website/app/app/lnd_client/admin/channels_model_view.py
@@ -2,10 +2,14 @@ from flask import flash
 from flask_admin.babel import gettext
 from flask_admin.model.ajax import AjaxModelLoader, DEFAULT_PAGE_SIZE
 from flask_admin.model.fields import AjaxSelectField
+from grpc import StatusCode
 
 from app.lnd_client.admin.lnd_model_view import LNDModelView
-from app.lnd_client.grpc_generated.rpc_pb2 import OpenChannelRequest, Channel, \
+from app.lnd_client.grpc_generated.rpc_pb2 import (
+    Channel,
+    OpenChannelRequest,
     Peer
+)
 
 
 class PeersAjaxModelLoader(AjaxModelLoader):
@@ -20,7 +24,8 @@ class PeersAjaxModelLoader(AjaxModelLoader):
         return model.pub_key, model.pub_key + '@' + model.address
 
     def get_one(self, pk):
-        pass
+        return [r for r in LNDModelView(Channel).ln.get_peers()
+                if r.pub_key == pk][0]
 
     def get_list(self, query, offset=0, limit=DEFAULT_PAGE_SIZE):
         pub_keys = LNDModelView(Channel).ln.get_peers()
@@ -28,7 +33,8 @@ class PeersAjaxModelLoader(AjaxModelLoader):
 
 
 class ChannelsModelView(LNDModelView):
-    peer_ajax_loader = PeersAjaxModelLoader('node_pubkey_string', options=None, model=Peer,
+    peer_ajax_loader = PeersAjaxModelLoader('node_pubkey_string', options=None,
+                                            model=Peer,
                                             placeholder='Select node pubkey')
 
     can_create = True
@@ -42,6 +48,8 @@ class ChannelsModelView(LNDModelView):
         'node_pubkey_string': peer_ajax_loader
     }
 
+
+
     def scaffold_form(self):
         form_class = super(ChannelsModelView, self).scaffold_form()
         old = form_class.node_pubkey_string
@@ -49,18 +57,19 @@ class ChannelsModelView(LNDModelView):
                                      label='node_pubkey_string',
                                      allow_blank=True,
                                      description=old.kwargs['description'])
+        form_class.local_funding_amount.kwargs['default'] = 60000
+        form_class.push_sat.kwargs['default'] = 0
+        form_class.target_conf.kwargs['default'] = 3
+        form_class.sat_per_byte.kwargs['default'] = 250
+        form_class.min_htlc_msat.kwargs['default'] = 1
         form_class.node_pubkey_string = ajax_field
         return form_class
 
     def create_model(self, form):
         data = form.data
-        data['node_pubkey_string'] = form.node_pubkey_string.raw_data[0]
+        data['node_pubkey_string'] = data['node_pubkey_string'].pub_key
         try:
             response = self.ln.open_channel(**data)
-            if hasattr(response, '_state') and response._state.details:
-                flash(gettext(response._state.details), 'error')
-                return False
-
         except Exception as exc:
             if hasattr(exc, '_state'):
                 flash(gettext(exc._state.details), 'error')
@@ -68,6 +77,10 @@ class ChannelsModelView(LNDModelView):
                 flash(gettext(str(exc)))
             return False
 
-        new_channel = [c for c in self.ln.get_channels()
-                       if c.node_pubkey_string == data['node_pubkey_string']][0]
-        return new_channel
+        if response.code() == StatusCode.UNKNOWN:
+            flash(gettext(response._state.details), 'error')
+            return False
+        else:
+            new_channel = [c for c in self.ln.get_channels()
+                           if c.remote_pubkey == data['node_pubkey_string']][0]
+            return new_channel
